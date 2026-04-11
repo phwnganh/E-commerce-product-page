d0772342f3b2fdbaef3418c7088d1da127ae1588
phwnganh <phuonganh25102003@gmail.com>
2026-04-12 00:37:35 +0700

    fix: responsive header section ui

--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,11 +1,15 @@
 import Logo from '../../assets/logo.svg'
+import MenuIcon from '../../assets/icon-menu.svg'
 const Menu = () => {
     return (
         <nav aria-label={"menu-site"} className={"flex items-center gap-14"}>
+            <button type={"button"} className={"flex md:hidden justify-center items-center"}>
+                <img src={MenuIcon} alt="Menu"/>
+            </button>
             <div className={"flex justify-center items-center"}>
                 <img src={Logo} alt="Logo" />
             </div>
-            <div className={"hidden sm:flex items-center gap-8"}>
+            <div className={"hidden md:flex items-center gap-8"}>
                 <a href={"/collections"} className={"text-grey-500 text-preset-4 leading-preset-4"}>Collections</a>
                 <a href={"/men"} className={"text-grey-500 text-preset-4 leading-preset-4"}>Men</a>
                 <a href={"/women"} className={"text-grey-500 text-preset-4 leading-preset-4"}>Women</a>
